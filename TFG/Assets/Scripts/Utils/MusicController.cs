cb3fe4024c4284f443d22f9231437556d05de4e4
Joshua García Palacios <Joshi666bcn@hotmail.com>
2015-03-04 20:12:38 +0100

    Commit
    Add:
    - New animation reload shotgun.
    - Update animation throw grenade.
    - Update animation reload handgun.
    - Add music effects

---
+++ TFG/Assets/Scripts/Utils/MusicController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MusicController : MonoBehaviour{
+
+    public AudioClip[] sound;
+
+    void Awake() 
+    {
+        if (gameObject.GetComponent<AudioSource>() == null)
+            gameObject.AddComponent<AudioSource>();
+    }
+
+    public void Play(int track) 
+    {
+        if(track <= sound.Length -1)
+            audio.PlayOneShot(sound[track]);
+    }
+}
